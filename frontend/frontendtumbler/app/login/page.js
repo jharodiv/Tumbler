f742950c64c44baffb7e8289cd66bd84288c38a9
Jem Centino <138832632+jharodiv@users.noreply.github.com>
2025-11-25 21:04:37 +0800

    login authentication logic fully works

--- frontend/frontendtumbler/app/login/page.js
+++ frontend/frontendtumbler/app/login/page.js
@@ -16,13 +16,26 @@ export default function LoginPage(){
         setError("");
 
         try {
-            const response = await axios.post("http://127.0.0.1:8000/login/", {
-                username,
-                password,
+            const response = await fetch("http://127.0.0.1:8000/login/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            }),
             });
 
-            localStorage.setItem("access_token", response.data.access);
-            localStorage.setItem("refresh_token", response.data.refresh);
+            if(!response.ok){
+                setError("Invalid Username or password");
+                return;
+            }
+
+            const data = await response.json();
+
+            localStorage.setItem("access_token",data.access);
+            localStorage.setItem("refresh_token",data.refresh);
 
             router.push("/dashboard");
         } catch (err){
@@ -32,16 +45,16 @@ export default function LoginPage(){
     };
 
     return(
-        <div className = "flex items-center justify-center min-h-screen bg-grat-100">
+        <div className = "flex items-center justify-center min-h-screen bg-gray-100">
             <div className = "w-full max-w-md p-8 bg-white rounded shadow">
-                <h2 className = "text-2xl font-bold mb-6 text-center"> Login </h2>
+                <h2 className = "text-2xl font-bold mb-6 text-center text-black"> Login </h2>
 
                 { error && (
                     <p className = "text-red-500 text-center mb-4">{error}</p>
                 )}
                 <form onSubmit={handleLogin} className="space-y-4">
-                    <input type = "text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} className="w-full p-3 border rounded" required/>
-                    <input type="text" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full p-3 border rounded" required/>
+                    <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} className="w-full p-3 border rounded text-black" required/>
+                    <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full p-3 border rounded text-black" required/>
                     <button type="submit" className="w-full bg-blue-600 text-white p-3 rounded hover:bg-blue-700 transition"> Login </button>
                 </form>
             </div>
--- tumbler/tumbler/settings.py
+++ tumbler/tumbler/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api_v1',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'tumbler.urls'
@@ -117,6 +119,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Static files (CSS, JavaScript, Images)
