00784266bf517846e516f2c81b5683202eefa678
Jem Centino <138832632+jharodiv@users.noreply.github.com>
2025-12-10 16:15:12 +0800

    Added Password hashing and unique username at backend

--- tumbler/api_v1/views.py
+++ tumbler/api_v1/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .serializers import RegisterSerializer 
 from rest_framework import status
