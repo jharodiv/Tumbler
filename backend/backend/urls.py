b270d708ef9f3d2b71f99197394fcff7d9f5cd94
Jem Harold Centino <138832632+jharodiv@users.noreply.github.com>
2026-01-20 07:13:08 +0000

    codespace

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -9,10 +9,9 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-
     #JWT
-    path('api/token', TokenObtainPairView.as_view(), name ='token_obtain_pair'),
-    path('api/token/refresh', TokenRefreshView.as_view(), name = 'token_refresh'),
+    path('api/token/', TokenObtainPairView.as_view(), name ='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name = 'token_refresh'),
 
     #users
     path('api/user/', include('user.urls')),
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/"
+    //baseURL: "http://127.0.0.1:8000/api/"
+    baseURL: "https://musical-trout-r4g47wqvp549cqww-5173.app.github.dev/api/"
 });
 
 api.interceptors.request.use((config) =>
