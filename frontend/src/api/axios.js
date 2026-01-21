685309e629f888ab274781c789a1403986faa837
Jem Harold Centino <138832632+jharodiv@users.noreply.github.com>
2026-01-21 05:31:46 +0000

    react + axios + django has successfully connected using codespace

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 const api = axios.create({
     //baseURL: "http://127.0.0.1:8000/api/"
-    baseURL: "https://musical-trout-r4g47wqvp549cqww-5173.app.github.dev/api/"
+    baseURL: "https://psychic-space-acorn-4j7jq659v7q524v5-8000.app.github.dev/api/",
+    headers:{
+        "Content-Type": "application/json"
+    }
 });
 
 api.interceptors.request.use((config) =>
