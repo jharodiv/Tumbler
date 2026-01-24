b0e4d60e2561949f93487c6c2aee52dba68189e3
Jem Centino <138832632+jharodiv@users.noreply.github.com>
2026-01-24 14:42:09 +0800

    CRUD assets at frontend

---
+++ frontend/src/assets/AssetCreate.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { createAsset } from "./assetServices";
+
+function AssetCreate( {onCreated} ){
+    const [name, setName] = useState = ("");
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        await createAsset({name});
+        setName("");
+        onCreated()
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input placeholder="Asset Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)} 
+            />
+            <button>Create</button>
+        </form>
+    );
+}
