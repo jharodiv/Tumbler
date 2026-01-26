c4e5547c2d727111de7f7ff1d37e252d1aa011a3
Jem Centino <138832632+jharodiv@users.noreply.github.com>
2026-01-26 21:05:56 +0800

    fixed the routing in the frontend for successful login

--- frontend/src/auth/protectedRoute.jsx
+++ frontend/src/auth/protectedRoute.jsx
@@ -9,7 +9,7 @@ export default function ProtectedRoute({children})
 
     if(isAuthenticated)
     {
-        return <Navigate to="/" replace />;
+        return <Navigate to="/assets" replace />;
     }
 
     return children;
