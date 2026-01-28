6b81be0a83101b537d6e584428d4c748ee88a3af
Jem Centino <138832632+jharodiv@users.noreply.github.com>
2026-01-28 22:56:09 +0800

    Frontend Bug Assets not showing Fixed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -7,16 +7,13 @@ const api = axios.create({
     }
 });
 
-api.interceptors.request.use((config) =>
-{
-    const token = localStorage.getItem("access");
-    if (token)
-    {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
+export const attachToken = (token) =>{
+    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+};
 
-    return config;
-});
+export const removeToken = () =>{
+    delete api.defaults.headers.common["Authorization"];
+}
 
 export default api;
 
