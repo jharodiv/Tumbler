4f633c835e9bb6bbc3a460dac537ed63256002ca
Jem Centino <138832632+jharodiv@users.noreply.github.com>
2026-02-16 22:00:29 +0800

    added registration form and route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./auth/Login";
 import AssetList from "./assets/AssetList";
 import ProtectedRoute from "./auth/protectedRoute";
+import Registration from "./auth/registration";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
         <Route
           path="/assets"
           element={
